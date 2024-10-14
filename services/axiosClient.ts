c3cc86f7f85dda0c2cc328dd7c3ba032ce8f928f
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-14 12:57:06 +0800

    Change baseurl back to not ngrok

--- services/axiosClient.ts
+++ services/axiosClient.ts
@@ -1,29 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: 'https://fb98-14-100-103-99.ngrok-free.app/api', //TODO: change to API gateway; hardcoded for now
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true
+  baseURL: "localhost:9000/api", //TODO: change to API gateway; hardcoded for now
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
 // Response interceptor for handling errors globally
 axiosClient.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response) {
-            if (error.response.status === 401) {
-                console.error('Unauthorized access.');
-                // router.push(/login);
-            } else if (error.response.status === 500) {
-                console.error('Server error, please try again later.');
-            }
-        } else {
-            console.error('Network error:', error.message);
-        }
-        return Promise.reject(error);
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        console.error("Unauthorized access.");
+        // router.push(/login);
+      } else if (error.response.status === 500) {
+        console.error("Server error, please try again later.");
+      }
+    } else {
+      console.error("Network error:", error.message);
     }
+    return Promise.reject(error);
+  },
 );
 
 export default axiosClient;
