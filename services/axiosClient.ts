2bbd066838e6488ea8712462ff325f90dc2f609b
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-14 13:04:49 +0800

    Environmentalize baseUrl variable

--- services/axiosClient.ts
+++ services/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "localhost:9000/api", //TODO: change to API gateway; hardcoded for now
+  baseURL: process.env.https://example.com/api, //TODO: change to API gateway; hardcoded for now
   headers: {
     "Content-Type": "application/json",
   },
