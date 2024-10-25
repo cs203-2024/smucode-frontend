7670266761eca339697a8a60a4f03ce173483f72
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2024-10-25 20:56:28 +0800

    Hotfix axios baseurl

--- services/axiosClient.ts
+++ services/axiosClient.ts
@@ -2,9 +2,10 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosClient = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: "http://localhost:9000/api",
+  //   baseURL: "http://localhost:8000/api",
     headers: {
-        'Authorization': `Bearer ${'authToken'}`,
+        // 'Authorization': `Bearer ${'authToken'}`,
         "Content-Type": "application/json",
     },
   withCredentials: true,
