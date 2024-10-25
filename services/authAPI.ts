e1af1a953746d6e21ca997207fe76a1aef4321cc
gavwyh <121929957+gavwyh@users.noreply.github.com>
2024-10-25 11:58:20 +0800

    Refactor user api and add auth api

---
+++ services/authAPI.ts
@@ -0,0 +1,71 @@
+import axiosClient from "./axiosClient";
+import { User } from "@/components/types";
+import Cookies from "js-cookie";
+
+//interface for user credentials (used for login and signup)
+interface UserCredentials {
+  username: string;
+  password: string;
+}
+
+//interface for login response
+interface LoginResponse {
+  message: string;
+  userDTO: User;
+  token: string;
+}
+
+//interface for signup response
+interface SignupResponse {
+  userDTO: User;
+}
+
+export const login = async (
+  credentials: UserCredentials,
+): Promise<LoginResponse> => {
+  try {
+    const response = await axiosClient.post<LoginResponse>(
+      "/auth/login",
+      credentials,
+    );
+    const { token } = response.data;
+
+    //store the jwt token in cookies
+    Cookies.set("authToken", token, { expires: 1, path: "/" });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error logging in:", error);
+    throw error;
+  }
+};
+
+export const signup = async (
+  newUser: Omit<User, "id">,
+): Promise<SignupResponse> => {
+  try {
+    const response = await axiosClient.post<SignupResponse>(
+      "/auth/signup",
+      newUser,
+    );
+
+    return response.data;
+  } catch (error: any) {
+    console.error("Error signing up:", error.response?.data || error.message);
+    throw error.response?.data || error;
+  }
+};
+
+export const logout = async (): Promise<string> => {
+  try {
+    const response = await axiosClient.post<string>("/auth/logout");
+
+    // Remove the JWT token
+    Cookies.remove("authToken");
+
+    return response.data;
+  } catch (error) {
+    console.error("Error logging out:", error);
+    throw error;
+  }
+};
