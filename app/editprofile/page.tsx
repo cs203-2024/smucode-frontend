4bb8193fc73b98762334ce8eff7436966c9864ae
ChampionGH <158540713+ChampionGH@users.noreply.github.com>
2024-10-28 20:11:26 +0800

    Improved ProfilePage and EditProfile

--- app/editprofile/page.tsx
+++ app/editprofile/page.tsx
@@ -12,6 +12,7 @@ import { useUserContext } from "@/context/UserContext";
 import * as Tooltip from "@radix-ui/react-tooltip";
 import Link from "next/link";
 import ChangePasswordForm from "@/components/ChangePasswordForm";
+import { deleteAccount } from "@/services/authAPI";
 
 const EditProfilePage: React.FC = () => {
   const { user } = useUserContext();
@@ -19,14 +20,7 @@ const EditProfilePage: React.FC = () => {
 
   const handleDeleteAccount = async () => {
     try {
-      const response = await fetch("/delete-account", {
-        method: "DELETE",
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to delete account");
-      }
-
+      await deleteAccount();
       alert("Account deleted successfully");
       // Redirect to login page after account deletion
       window.location.href = "/login";
@@ -50,7 +44,7 @@ const EditProfilePage: React.FC = () => {
     <Tooltip.Provider>
       <div className="pt-10">
         {/* Add padding-top to push content below the navbar */}
-        <div className="container mx-auto p-4 md:p-6 lg:p-8">
+        <div className="container mx-auto p-4 md:p-6 lg:p-8 max-w-lg">
           <div className="flex-1 space-y-4 p-8 pt-6">
             <Link href={`/profile`}>
               <Button className="bg-gray-500 text-white hover:bg-gray-600 px-8 py-2 mb-4">
