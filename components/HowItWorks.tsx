4aeee0f01917a0fb21d00b2c37a4e553514dcf8e
ChampionGH <158540713+ChampionGH@users.noreply.github.com>
2024-11-11 16:09:30 +0800

    Updated Home Page

--- components/HowItWorks.tsx
+++ components/HowItWorks.tsx
@@ -1,5 +1,10 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { MedalIcon, MapIcon, PlaneIcon, GiftIcon } from "@/components/IconsHowItWorks";
+import {
+  MedalIcon,
+  MapIcon,
+  PlaneIcon,
+  GiftIcon,
+} from "@/components/IconsHowItWorks";
 
 interface FeatureProps {
   icon: JSX.Element;
@@ -18,7 +23,7 @@ const features: FeatureProps[] = [
     icon: <MapIcon />,
     title: "Community",
     description:
-      "Admins create tournaments, while participants join and compete. SMUCode fosters a collaborative and competitive community.",
+      "Admins create tournaments, while participants join and compete. BrawlCode fosters a collaborative and competitive community.",
   },
   {
     icon: <PlaneIcon />,
@@ -36,30 +41,21 @@ const features: FeatureProps[] = [
 
 export const HowItWorks = () => {
   return (
-    <section
-      id="howItWorks"
-      className="container text-center py-24 sm:py-32"
-    >
+    <section id="howItWorks" className="container text-center py-24 sm:py-32">
       <h2 className="text-3xl md:text-4xl font-bold ">
-        How It{" "}
-        <span style={{ color: '#16a34a' }}>
-          Works{" "}
-        </span>
+        How It <span style={{ color: "#16a34a" }}>Works </span>
         Step-by-Step Guide
       </h2>
       <p className="md:w-3/4 mx-auto mt-4 mb-8 text-xl text-muted-foreground">
-      Discover how to create, join, and manage tournaments effortlessly.
+        Discover how to create, join, and manage tournaments effortlessly.
       </p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {features.map(({ icon, title, description }) => (
-          <Card
-            key={title}
-            className="bg-muted/50"
-          >
+          <Card key={title} className="bg-muted/50">
             <CardHeader>
               <CardTitle className="grid gap-4 place-items-center text-2xl md:text-2xl">
-                <span style={{ color: '#16a34a' }}>{icon}</span>
+                <span style={{ color: "#16a34a" }}>{icon}</span>
                 {title}
               </CardTitle>
             </CardHeader>
